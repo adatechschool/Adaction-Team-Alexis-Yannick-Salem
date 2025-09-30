c575314bdbf0bd7f30b1e0b8b863c831d473a302
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-09-30 15:36:44 +0200

    add routes get assocition, benevole and post signup

--- back_end/index.js
+++ back_end/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {Pool} from "pg"
+import { Pool } from "pg";
+import bcrypt from "bcrypt";
 
 
 dotenv.config();
@@ -15,39 +16,90 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.json("hello world cc");
+});
 
-app.get('/',(req,res)=>{
-    res.json("hello world cc")
-})
-// route ville
-app.get('/ville',async (req,res)=>{
-  
+// route get associations
+
+app.get("/Associations", async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM ville')
-    res.json(result.rows)
+    const reslt = await db.query(`SELECT name, sigle FROM associations`);
+    res.status(200).json(reslt.rows);
   } catch (error) {
-    console.error("Erreur lors de la recupération des villes:", error)
-    res.status(500).json("Erreur serveur")
-  }
-})
-// route dechets
-
-app.get('/dechets', async(req,res)=>{
-
-  try {
-    const reslt = await db.query('SELECT name ,score, icon FROM dechets');
-    res.json(reslt.rows);
-    
-  } catch (error) {
-    console.error('Erreur lors de la recuperation des dechets',error);
+    console.error("Erreur lors de la recuperation des associations", error);
     res.status(500).json("Erreur serveur");
-
   }
-})
+});
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
-})
+// route get Benvole
+app.get("/Benevole", async (req, res) => {
+  try {
+    const reslt = await db.query(`SELECT * FROM benevoles `);
+    res.status(200).json(reslt.rows)
+  } catch (error) {
+    console.error(
+      "Erreur lors de la recuperation des donnees benevoles",
+      error
+    );
+    res.status(500).json("Erreur serveur");
+  }
+});
+
+// route get ville
+app.get("/Ville", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM ville");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Erreur lors de la recupération des villes:", error);
+    res.status(500).json("Erreur serveur");
+  }
+});
+
+// route get dechets
+
+app.get("/Dechets", async (req, res) => {
+  try {
+    const reslt = await db.query("SELECT name ,score, icon FROM dechets");
+    res.json(reslt.rows);
+  } catch (error) {
+    console.error("Erreur lors de la recuperation des dechets", error);
+    res.status(500).json("Erreur serveur");
+  }
+});
+
+// route singup
+
+app.post("/Signup", async (req, res) => {
+  try {
+    const { username, first_name,  password, id_ville} = req.body;
+    if (!username || !password || !first_name) {
+      return res
+        .status(400)
+        .json({ error: "tous les champs doit etre remplis" });
+    }
+    const check = await db.query(
+      "SELECT id FROM benevoles WHERE username = $1",
+      [username]
+    );
+    if (check.rows.length > 0)
+      return res.status(400).json({ error: "Nom deja existant" });
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const reslt = await db.query(
+      `INSERT INTO benevoles (username, password, first_name,  points_collectes,  id_ville, date_creation)
+      VALUES ('${username}', '${hashedPassword}', '${first_name}',  0,  ${id_ville}, current_timestamp)`);
+    res.status(201).json(reslt.rows[0]);
+  } catch (error) {
+    console.error("Erreur signup:", error);
+    res.status(500).json({ error: "Erreur serveur" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
